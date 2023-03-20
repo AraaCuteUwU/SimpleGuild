f543f9d5c9f1012e92cb805ed9d17adcd0d3c6e5
AraaCuteUwU <71029264+AraaCuteUwU@users.noreply.github.com>
2023-03-20 12:10:53 +0800

    Add files via upload

---
+++ src/FiraAja/SimpleGuild/SimpleGuild.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace FiraAja\SimpleGuild;
+
+use pocketmine\plugin\PluginBase;
+
+class SimpleGuild extends PluginBase {
+
+    public function onEnable(): void {
+        //TODO
+    }
+}
